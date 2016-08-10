f07201b47b73e502c44e853e2114aaf3d76da229
Victor Leroy <victorleroy@gmail.com>
2016-08-10 17:21:53 +0200

    MVP ok

--- Gemfile
+++ Gemfile
@@ -26,8 +26,7 @@ gem 'jbuilder', '~> 2.5'
 # gem 'redis', '~> 3.0'
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
-
-
+gem 'faker'
 # frontend gems
 gem 'bootstrap-sass'
 gem 'font-awesome-sass'
@@ -49,6 +48,8 @@ group :development do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'spring-watcher-listen', '~> 2.0.0'
+  gem 'better_errors'
+  gem 'binding_of_caller'
 end
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,41 +1,13 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
 Restaurant.destroy_all
-restaurants_attributes = [
-  {
-    name:         "Epicure au Bristol",
-    address:      "112 rue du Fg St-Honoré 75008 Paris",
-    phone_number:  "Face au jardin, on découvre une salle lumineuse... et la cuisine d'Éric Frechon.",
-    category:       "chinese",
-  },
-  {
-    name:         "La truffière",
-    address:      "4 rue Blainville 75005 Paris",
-    phone_number:  "Une valeur sûre que cette belle maison du 17e et les recettes de Jean-Christophe Rizet",
-    category:        "italian",
-  },
-  {
-    name:         "Le pré catelan",
-    address:      "route de Suresnes 75016 Paris",
-    phone_number:  "Oeil vif, geste sûr: impossible de distinguer, dans les créations de Frédéric Anton...",
-    category:       "japanese",
-  },
-  {
-    name:         "L'acajou'",
-    address:      "rue Victor Hugo 75016 Paris",
-    phone_number:  "stylax...",
-    category:       "french",
-  },
-  {
-    name:         "Soya",
-    address:      "rue ... 75011 Paris",
-    phone_number:  "coolax...",
-    category:       "belgian"
-  }
-]
-restaurants_attributes.each { |params| Restaurant.create!(params) }
+5.times do
+  restaurant = Restaurant.new(
+    name:         Faker::Pokemon.name,
+    address:      Faker::Address.street_address,
+    phone_number:  Faker::PhoneNumber.phone_number,
+    category:       ["chinese", "italian", "japanese", "french", "belgian"].sample
+  )
+  restaurant.save
+end
+
+# restaurants_attributes.each { |params| Restaurant.create!(params) }
